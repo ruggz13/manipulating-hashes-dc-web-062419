e9d54f35cd5346595e7121d669654ba6d54611c1
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-21 19:03:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,8 +17,8 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.each do |flavor|
-          flavor.remove ("strawberry")
+        value.delete_if do |flavor|
+          flavor == "strawberry"
         end
       end
     end
